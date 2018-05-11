93b0539cb183a15391ea7837175d127f63719699
flpsno <flpsno@hotmail.com>
2018-05-11 11:19:57 -0300

    Adicionada as classes entities e repositories

---
+++ ACM.Win/ACM.BL/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string EmaiAddress { get; set; }
+
+        public string FirsName{ get; set; }
+
+        public string LastName { get; set; }
+    }
+}
